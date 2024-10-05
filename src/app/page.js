0431c30af4f44a67568786ea58910eb1d1a8a858
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-05 21:14:19 +0530

    few data updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,5 +3,8 @@
 import CoinScene from '@/components/CoinScene';
 
 export default function HomePage() {
-  return <CoinScene shouldFlip={true} />;
+  return <>
+  <h1 className='text-2xl font-semibold text-white text-center'>here is 3d model addded soon</h1>
+  <CoinScene shouldFlip={true} />
+  </>
 }
