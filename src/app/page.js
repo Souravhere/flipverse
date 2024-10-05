f4a0da52d5487f435b1dca7bdf7a72d547f951ea
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-05 21:18:21 +0530

    few explaniationa  added about this project

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import CoinScene from '@/components/CoinScene';
 export default function HomePage() {
   return <>
   <h1 className='text-2xl font-semibold text-white text-center'>here is 3d model addded soon</h1>
+  <p>this is under construction here a coin 3d model added and it will toss and it will show the tail and head with easeing animation </p>
   <CoinScene shouldFlip={true} />
   </>
 }
